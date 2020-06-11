9b50ae4039fcb3ca2c4a0bed417e2911203a3459
Stefan <64359359+SteveMcT@users.noreply.github.com>
2020-06-11 09:04:49 +0200

    Initial commit

---
+++ Heart.java
@@ -0,0 +1,48 @@
+package Herz;
+
+import java.util.Scanner;
+
+public class Heart {
+
+    public static void main(String[] args) {
+
+        Heart.herz();
+
+    }
+
+    public static void herz(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        int i,j,n;
+        System.out.println("Enter Value");
+        n = scanner.nextInt();
+
+        for(i = n/2;i<=n; i+=2){
+            for(j = 1;j<n-i;j+=2){
+                System.out.print(" ");
+            }
+            for(j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(j = 1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(i = n; i>=1;i--){
+            for(j=i;j<n;j++){
+                System.out.print(" ");
+            }
+            for(j=1; j<=(i*2)-1; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Herz
+ This is a "Simple" Algorythm which creates a heart. It was lot of work and I'm satisfied with the result. 
